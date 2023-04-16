4cfb2e6e509a73a767dcc3d92dcfdac2c9bd6f69
Catlak Profesor <progesor@gmail.com>
2023-04-16 16:42:32 +0300

    Added Saveable

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,11 +1,12 @@
 using ProgesorCreating.RPG.Core;
+using ProgesorCreating.RPG.Saving;
 using UnityEngine;
 using UnityEngine.AI;
 
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.RPG.Movement
 {
-    public class Mover : MonoBehaviour,IAction
+    public class Mover : MonoBehaviour,IAction,ISaveable
     {
         [SerializeField] public Transform target;
         [SerializeField] private float maxSpeed = 4.1f;
@@ -62,6 +63,19 @@ namespace ProgesorCreating.RPG.Movement
         public void FootR()
         {
         
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            _navMeshAgent.enabled = false;
+            transform.position = position.ToVector();
+            _navMeshAgent.enabled = true;
         }
     }
 
