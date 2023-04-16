ed77c03a8174aafd59e81978e6b6364b45cc882d
Catlak Profesor <progesor@gmail.com>
2023-04-16 16:10:36 +0300

    SavingWrapper Moved SceneManagement

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using ProgesorCreating.RPG.Saving;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        private const string DefaultSaveFile = "save";
+
+        [SerializeField] private float fadeInTime = 0.2f;
+
+        private IEnumerator Start()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(DefaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+        }
+
+        public void Load()
+        {
+            GetComponent<SavingSystem>().Load(DefaultSaveFile);
+        }
+
+        public void Save()
+        {
+            GetComponent<SavingSystem>().Save(DefaultSaveFile);
+        }
+    }
+}
