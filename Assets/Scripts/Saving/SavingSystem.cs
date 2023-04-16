0fdd934890676bad4cd7dc55611bc2a9b2bbdbf0
Catlak Profesor <progesor@gmail.com>
2023-04-16 09:39:54 +0300

    Saving system fundamentals

---
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            print("Saving to "+saveFile);
+        }
+        
+        public void Load(string saveFile)
+        {
+            print("Loading from "+saveFile);
+        }
+    }
+}
---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        private const string defaultSaveFile = "save";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                GetComponent<SavingSystem>().Save(defaultSaveFile);
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                GetComponent<SavingSystem>().Load(defaultSaveFile);
+            }
+        }
+    }
+}
