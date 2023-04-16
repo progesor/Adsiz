7ba29ae1d76a52f1b77eda4441e6009bbfe159ba
Catlak Profesor <progesor@gmail.com>
2023-04-16 13:45:10 +0300

    Update SaveableEntity.cs

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,13 +1,17 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] private string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
@@ -20,5 +24,22 @@ namespace ProgesorCreating.RPG.Saving
         {
             print("Restoring state for " + GetUniqueIdentifier());
         }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+#endif
     }
 }
