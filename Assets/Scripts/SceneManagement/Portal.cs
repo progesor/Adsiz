b115a54fb21321f0fea5148dd59caaaf7cbb3dba
Catlak Profesor <progesor@gmail.com>
2023-04-16 06:07:47 +0300

    New Sandbox map and Basic of portal

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GameObject().CompareTag("Player"))
+            {
+                
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            yield return null;
+        }
+    }
+}
