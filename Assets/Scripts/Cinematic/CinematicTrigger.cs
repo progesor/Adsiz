b8294e3993b070ca5d4c403c94c23f8d0558b05c
Catlak Profesor <progesor@gmail.com>
2023-04-16 04:59:56 +0300

    Change Player Camera and Add Cinematics basics

---
+++ Assets/Scripts/Cinematic/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Cinematic
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool _alreadyTriggered;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_alreadyTriggered && other.gameObject.CompareTag("Player"))
+            {
+                _alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
