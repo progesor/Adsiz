61b78c38fa41441406bb1062599e0f89221cd4a2
Catlak Profesor <progesor@gmail.com>
2023-04-16 07:58:06 +0300

    Portal Object and Desinaion

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 // ReSharper disable once CheckNamespace
@@ -8,19 +9,57 @@ namespace ProgesorCreating.RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P
+        }
+        
         [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private DestinationIdentifier destination;
         private void OnTriggerEnter(Collider other)
         {
             if (other.GameObject().CompareTag("Player"))
             {
-                
+                StartCoroutine(Transition());
             }
         }
 
         private IEnumerator Transition()
         {
-            SceneManager.LoadScene(sceneToLoad);
-            yield return null;
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+            
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal==this)continue;
+                if (portal.destination!=destination)continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
