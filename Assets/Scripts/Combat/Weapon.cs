32796f03977fa77745091f2975df6b5d480779d8
Catlak Profesor <progesor@gmail.com>
2023-04-17 05:25:21 +0300

    Created Simple Weapon System

---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Combat
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+        [SerializeField] private AnimatorOverrideController animatorOverride;
+        [SerializeField] private GameObject weaponPrefab;
+
+        public void Spawn(Transform handTransform,Animator animator)
+        {
+            Instantiate(weaponPrefab, handTransform);
+            animator.runtimeAnimatorController = animatorOverride;
+        }
+    }
+}
