5c8d5fd08173a7b8477224538188336f7a40fee9
Catlak Profesor <progesor@gmail.com>
2023-04-17 18:48:09 +0300

    Create DestroyAfterEffect.cs

---
+++ Assets/Scripts/Core/DestroyAfterEffect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Core
+{
+    public class DestroyAfterEffect : MonoBehaviour
+    {
+        // private void Update()
+        // {
+        //     if (!GetComponent<ParticleSystem>().IsAlive())
+        //     {
+        //         Destroy(gameObject);
+        //     }
+        // }
+        
+        [SerializeField] private float destroyTime = 1f;
+
+        private void Start()
+        {
+            Destroy(gameObject, destroyTime);
+        }
+    }
+}
