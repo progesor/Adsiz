8f382ea8f085a85c3cee93ab747adb46877bff45
Catlak Profesor <progesor@gmail.com>
2023-04-17 07:28:46 +0300

    Create WeaponPickup.cs

---
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Combat
+{
+    public class WeaponPickup : MonoBehaviour
+    {
+        [SerializeField] private Weapon weapon;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Fighter>().EquipWeapon(weapon);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
