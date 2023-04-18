93782dd993a44d9bb86da422cdfa7700e3ad803f
Catlak Profesor <progesor@gmail.com>
2023-04-18 21:50:51 +0300

    Display To Level

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        
+        
+        private BaseStats _baseStats;
+
+        private void Awake()
+        {
+            _baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        private void Update()
+        {
+            GetComponent<TextMeshProUGUI>().SetText(String.Format("{0:0}",_baseStats.GetLevel()));
+        }
+    }
+}
