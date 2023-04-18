c26c8707fdad1df5f78d02fcb7347af990477eb7
Catlak Profesor <progesor@gmail.com>
2023-04-18 08:51:37 +0300

    Updated Weapon Pickup

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.RPG.Combat
@@ -6,13 +8,36 @@ namespace ProgesorCreating.RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
+        [SerializeField] private float respawnTime = 5f;
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+            
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+
+            GetComponent<Rigidbody>().useGravity = shouldShow;
+            GetComponent<Collider>().enabled = shouldShow;
+            
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
     }
 }
