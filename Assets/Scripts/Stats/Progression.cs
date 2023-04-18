19428e0f3106b585b70ff1fdfbf238f1325f2402
Catlak Profesor <progesor@gmail.com>
2023-04-18 12:31:49 +0300

    Create Progression.cs

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProgesorCreating.RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression",menuName = "Stats/New Progression",order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] private ProgressionCharacterClass[] characterClass = null;
+        
+        [Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] private CharacterClass characterClass;
+            [SerializeField] private float[] health;
+        }
+    }
+}
