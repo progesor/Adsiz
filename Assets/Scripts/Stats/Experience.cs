fb6ac288a407217190716bd82c11e2172000585f
Catlak Profesor <progesor@gmail.com>
2023-04-18 18:28:08 +0300

    Saving Experience Points

--- Assets/Scripts/Stats/Experience.cs
+++ Assets/Scripts/Stats/Experience.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+﻿using ProgesorCreating.RPG.Saving;
+using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.RPG.Stats
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour,ISaveable
     {
         [SerializeField] private float experiencePoints;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
