c37bc6b97f7600e4a684aa6f2466510256556b4e
Catlak Profesor <progesor@gmail.com>
2023-04-18 12:32:02 +0300

    Create BaseStats.cs

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1,99)]
+        [SerializeField] private int startingLevel = 1;
+        [SerializeField] private CharacterClass characterClass;
+        [SerializeField] private Progression progression;
+
+
+    }
+}
