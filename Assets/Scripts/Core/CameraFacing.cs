f6de900e28fe1d477632cede1d86a700892a7a41
Catlak Profesor <progesor@gmail.com>
2023-04-20 02:44:57 +0300

    In Game Ui

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
