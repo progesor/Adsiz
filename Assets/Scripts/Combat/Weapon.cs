f9bc771c03723064babfccafae24cdaaa84525bd
Catlak Profesor <progesor@gmail.com>
2023-04-20 15:54:23 +0300

    Boom Microphone Audio Listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private UnityEvent onHit;
         public void OnHit()
         {
-            print("Weapon Hit " + gameObject.name);
+            onHit.Invoke();
         }
     }
 }
