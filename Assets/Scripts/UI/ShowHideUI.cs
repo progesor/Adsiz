e3aca75022dcc46a6ec0899fe239dd55973b75e3
Catlak Profesor <progesor@gmail.com>
2023-04-21 05:33:33 +0300

    ShowHideUI

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.RPG.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField] private KeyCode toggleKey = KeyCode.Escape;
+        [SerializeField] private GameObject uiContainer;
+
+        private void Start()
+        {
+            uiContainer.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(toggleKey))
+            {
+                uiContainer.SetActive(!uiContainer.activeSelf);
+            }
+        }
+    }
+}
