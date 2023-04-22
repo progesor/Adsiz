ab53ae51c8b13a3dfb32237ba7baea1ac648625d
Catlak Profesor <progesor@gmail.com>
2023-04-22 17:50:37 +0300

    Created Clickable Pickup

---
+++ Assets/Scripts/Control/ClickablePickup.cs
@@ -0,0 +1,39 @@
+﻿using ProgesorCreating.Inventories;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class ClickablePickup :MonoBehaviour, IRaycastable
+    {
+        private Pickup _pickup;
+
+        private void Awake()
+        {
+            _pickup = GetComponent<Pickup>();
+        }
+
+        public CursorType getCursorType()
+        {
+            if (_pickup.CanBePickedUp())
+            {
+                return CursorType.Pickup;
+            }
+            else
+            {
+                return CursorType.FullPickup;
+            }
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _pickup.PickupItem();
+            }
+
+            return true;
+        }
+    }
+}
