6676859784313435dd36a5567e80adf500ab7ec6
Catlak Profesor <progesor@gmail.com>
2023-04-23 19:39:59 +0300

    Showing a custom editor and Openning asset callback

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 // ReSharper disable once CheckNamespace
 namespace ProgesorCreating.Dialogue
 {
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,7 +1,26 @@
-﻿namespace ProgesorCreating.Dialogue.Editor
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace ProgesorCreating.Dialogue.Editor
 {
-    public class DialogueEditor
+    public class DialogueEditor : EditorWindow
     {
-        
+        [MenuItem("Window/Dialogue Editor")]
+        public static void ShowEditorWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceId, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceId) as Dialogue;
+            if (dialogue!=null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
     }
 }
