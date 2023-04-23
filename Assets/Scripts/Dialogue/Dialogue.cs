8ac2c1fa1a4b1994e97e10704b642b5f59bb8670
Catlak Profesor <progesor@gmail.com>
2023-04-23 21:54:05 +0300

    GUI Basic 2

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -11,6 +12,8 @@ namespace ProgesorCreating.Dialogue
     {
         [SerializeField] private List<DialogueNode> nodes = new List<DialogueNode>();
 
+        private Dictionary<string, DialogueNode> _nodeLookup = new Dictionary<string, DialogueNode>();
+
 #if UNITY_EDITOR
         private void Awake()
         {
@@ -18,8 +21,19 @@ namespace ProgesorCreating.Dialogue
             {
                 nodes.Add(new DialogueNode());
             }
+            OnValidate();
         }
 #endif
+
+        private void OnValidate()
+        {
+            _nodeLookup.Clear();
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                _nodeLookup[node.uniqueID] = node;
+            }
+        }
+
         public IEnumerable<DialogueNode> GetAllNodes()
         {
             return nodes;
@@ -28,6 +42,17 @@ namespace ProgesorCreating.Dialogue
         public DialogueNode GetRootNode()
         {
             return nodes[0];
+        }
+
+        public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+        {
+            foreach (string childId in parentNode.children)
+            {
+                if (_nodeLookup.ContainsKey(childId))
+                {
+                    yield return _nodeLookup[childId];
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -10,7 +10,8 @@ namespace ProgesorCreating.Dialogue.Editor
     {
         private Dialogue _selectedDialogue;
         private GUIStyle _nodeStyle;
-        private bool _dragging;
+        private DialogueNode _draggingNode;
+        private Vector2 _draggingOffset;
         
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
@@ -61,7 +62,11 @@ namespace ProgesorCreating.Dialogue.Editor
                 ProcessEvents();
                 foreach (DialogueNode node in _selectedDialogue.GetAllNodes())
                 {
-                    OnGUINode(node);
+                    DrawConnections(node);
+                }
+                foreach (DialogueNode node in _selectedDialogue.GetAllNodes())
+                {
+                    DrawNode(node);
                 }
             }
             
@@ -69,23 +74,27 @@ namespace ProgesorCreating.Dialogue.Editor
 
         private void ProcessEvents()
         {
-            if (Event.current.type == EventType.MouseDown && !_dragging)
+            if (Event.current.type == EventType.MouseDown && _draggingNode==null)
             {
-                _dragging = true;
+                _draggingNode = GetNodeAtPoint(Event.current.mousePosition);
+                if (_draggingNode!=null)
+                {
+                    _draggingOffset = _draggingNode.rect.position - Event.current.mousePosition;
+                }
             }
-            else if (Event.current.type == EventType.MouseDrag && _dragging)
+            else if (Event.current.type == EventType.MouseDrag && _draggingNode!=null)
             {
                 Undo.RecordObject(_selectedDialogue,"Move Dialogue Node");
-                _selectedDialogue.GetRootNode().rect.position = Event.current.mousePosition;
+                _draggingNode.rect.position = Event.current.mousePosition + _draggingOffset;
                 GUI.changed = true;
             }
-            else if (Event.current.type == EventType.MouseUp && _dragging)
+            else if (Event.current.type == EventType.MouseUp && _draggingNode!=null)
             {
-                _dragging = false;
+                _draggingNode = null;
             }
         }
 
-        private void OnGUINode(DialogueNode node)
+        private void DrawNode(DialogueNode node)
         {
             GUILayout.BeginArea(node.rect, _nodeStyle);
             EditorGUI.BeginChangeCheck();
@@ -101,6 +110,33 @@ namespace ProgesorCreating.Dialogue.Editor
                 node.uniqueID = newUniqueID;
             }
             GUILayout.EndArea();
+        }
+
+        private void DrawConnections(DialogueNode node)
+        {
+            Vector3 startPosition = new Vector2(node.rect.xMax, node.rect.center.y);
+            foreach (DialogueNode childNode in _selectedDialogue.GetAllChildren(node))
+            {
+                Vector3 endPosition = new Vector2(childNode.rect.xMin, childNode.rect.center.y);
+                Vector3 controlPointOffset = endPosition - startPosition;
+                controlPointOffset.y = 0;
+                controlPointOffset.x *= 0.8f;
+                Handles.DrawBezier(startPosition, endPosition, startPosition+controlPointOffset, endPosition-controlPointOffset, Color.white, null, 4f);
+            }
+        }
+        
+        private DialogueNode GetNodeAtPoint(Vector2 point)
+        {
+            DialogueNode foundNode = null;
+            foreach (DialogueNode node in _selectedDialogue.GetAllNodes())
+            {
+                if (node.rect.Contains(point))
+                {
+                    foundNode = node;
+                }
+            }
+
+            return foundNode;
         }
     }
 }
