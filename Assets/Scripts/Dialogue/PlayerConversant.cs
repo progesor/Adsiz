28dd9c7f454522b020ea9716cceb6ec5e1459eb7
Catlak Profesor <progesor@gmail.com>
2023-04-24 18:53:18 +0300

    Dialog (I am bored to add summary)

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] private Dialogue currentDialogue;
+
+        public string GetText()
+        {
+            if (currentDialogue==null)
+            {
+                return string.Empty;
+            }
+
+            return currentDialogue.GetRootNode().GetText();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProgesorCreating.Dialogue;
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI AIText;
+        
+        private PlayerConversant _playerConversant;
+
+        private void Start()
+        {
+            _playerConversant = GameObject.FindWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.SetText(_playerConversant.GetText());
+        }
+    }
+}
