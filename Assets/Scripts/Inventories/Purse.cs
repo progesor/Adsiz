1a61916c1a74215cffdf598b316ddd724517eb24
Catlak Profesor <progesor@gmail.com>
2023-05-03 18:45:31 +0300

    Money Component

---
+++ Assets/Scripts/Inventories/Purse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.Inventories
+{
+    public class Purse : MonoBehaviour
+    {
+        [SerializeField] private float startingBalance = 400f;
+
+        private float _balance;
+
+        private void Awake()
+        {
+            _balance = startingBalance;
+        }
+
+        public float GetBalance()
+        {
+            return _balance;
+        }
+
+        public void UpdateBalance(float amount)
+        {
+            _balance += amount;
+        }
+    }
+}
--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -66,19 +66,23 @@ namespace ProgesorCreating.Shops
         public void ConfirmTransaction()
         {
             Inventory shopperInventory = _currentShopper.GetComponent<Inventory>();
-            if (shopperInventory==null)return;
+            Purse shopperPurse = _currentShopper.GetComponent<Purse>();
+            if (shopperInventory == null || shopperPurse == null) return;
 
-            Dictionary<InventoryItem, int> transactionSnapshot = new Dictionary<InventoryItem, int>(transaction);
-
-            foreach (InventoryItem item in transactionSnapshot.Keys)
+            foreach (ShopItem shopItem in GetAllItems())
             {
-                int quantity = transactionSnapshot[item];
+                InventoryItem item = shopItem.GetInventoryItem();
+                int quantity = shopItem.GetQuantityInTransaction();
+                float price = shopItem.GetPrice();
                 for (int i = 0; i < quantity; i++)
                 {
+                    if (shopperPurse.GetBalance()<price) break;
+                    
                     bool success = shopperInventory.AddToFirstEmptySlot(item, 1);
                     if (success)
                     {
                         AddToTransaction(item,-1);
+                        shopperPurse.UpdateBalance(-price);
                     }
                 }
                 
