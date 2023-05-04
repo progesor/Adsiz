67fc28a6fe2d8ce7f53dccfcd106400ab6a1c5e3
Catlak Profesor <progesor@gmail.com>
2023-05-04 19:06:18 +0300

    Calculating Price By Level

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -52,16 +52,17 @@ namespace ProgesorCreating.Shops
 
         public IEnumerable<ShopItem> GetAllItems()
         {
-            int shopperLevel = GetShopperLevel();
-            foreach (StockItemConfig config in stockConfig)
+            Dictionary<InventoryItem, float> prices = GetPrices();
+            Dictionary<InventoryItem, int> avilabilities = GetAvailabilities();
+            foreach (InventoryItem item in avilabilities.Keys)
             {
-                if (config.levelToUnlock>shopperLevel)continue;
-                
-                float price = GetPrice(config);
+                if (avilabilities[item]<=0)continue;
+
+                float price = prices[item];
                 int quantityInTransaction = 0;
-                _transaction.TryGetValue(config.item, out quantityInTransaction);
-                int availability = GetAvailability(config.item);
-                yield return new ShopItem(config.item, availability, price, quantityInTransaction); 
+                _transaction.TryGetValue(item, out quantityInTransaction);
+                int availability = avilabilities[item];
+                yield return new ShopItem(item, availability, price, quantityInTransaction); 
             }
         }
 
@@ -253,6 +254,48 @@ namespace ProgesorCreating.Shops
 
             return config.item.GetPrice() * (sellingPercentage / 100);
 
+        }
+
+        private Dictionary<InventoryItem, int> GetAvailabilities()
+        {
+            Dictionary<InventoryItem, int> availabilities = new Dictionary<InventoryItem, int>();
+
+            foreach (StockItemConfig config in GetAvailableConfigs())
+            {
+                if (!availabilities.ContainsKey(config.item))
+                {
+                    availabilities[config.item] = 0;
+                }
+                availabilities[config.item] += config.initialStock;
+            }
+            return availabilities;
+        }
+
+        private Dictionary<InventoryItem, float> GetPrices()
+        {
+            Dictionary<InventoryItem, float> prices = new Dictionary<InventoryItem, float>();
+
+            foreach (StockItemConfig config in GetAvailableConfigs())
+            {
+                if (!prices.ContainsKey(config.item))
+                {
+                    prices[config.item] = config.item.GetPrice();
+                }
+
+                prices[config.item] *= (1 - config.buyingDiscountPercentage / 100);
+            }
+            
+            return prices;
+        }
+
+        public IEnumerable<StockItemConfig> GetAvailableConfigs()
+        {
+            int shopperLevel = GetShopperLevel();
+            foreach (StockItemConfig config in stockConfig)
+            {
+                if (config.levelToUnlock>shopperLevel)continue;
+                yield return config;
+            }
         }
 
         private void SellItem(Inventory shopperInventory, Purse shopperPurse, InventoryItem item, float price)
