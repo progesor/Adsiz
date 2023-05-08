d6628e552cda004422035efaa8260a6d6b42256f
Catlak Profesor <progesor@gmail.com>
2023-05-08 04:05:21 +0300

    Animation Effect Strategy

---
+++ Assets/Scripts/Abilities/Effects/OrientToTargetEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProgesorCreating.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Orient To Target Effect",menuName = "Abilities/Effect/New Orient To Target Effect",order = 0)]
+    public class OrientToTargetEffect : EffectStrategy
+    {
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            data.GetUser().transform.LookAt(data.GetTargetedPoint());
+            finished();
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/Effects/TriggerAnimationEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace ProgesorCreating.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Trigger Animation Effect",menuName = "Abilities/Effect/New Trigger Animation Effect",order = 0)]
+    public class TriggerAnimationEffect : EffectStrategy
+    {
+        [SerializeField] private string animationTriggers;
+
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            Animator animator = data.GetUser().GetComponent<Animator>();
+            animator.SetTrigger(animationTriggers);
+            finished();
+        }
+    }
+}
