0a29fe5904b8ef66be5af40a43d708425e7a38bb
Catlak Profesor <progesor@gmail.com>
2023-05-09 04:51:18 +0300

    Trait Row UI Script

---
+++ Assets/Scripts/Stats/Trait.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.Stats
+{
+    public enum Trait
+    {
+        Strength,
+        Agility,
+        Dexterity,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+}
---
+++ Assets/Scripts/UI/TraitRowUI.cs
@@ -0,0 +1,40 @@
+﻿using ProgesorCreating.Stats;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.UI
+{
+    public class TraitRowUI : MonoBehaviour
+    {
+        [SerializeField] private Trait trait;
+        [SerializeField] private TextMeshProUGUI valueText;
+        [SerializeField] private Button minusButton;
+        [SerializeField] private Button plusButton;
+
+        private int _value;
+
+        private void Start()
+        {
+            minusButton.onClick.AddListener(()=>Allocate(-1));
+            plusButton.onClick.AddListener(()=>Allocate(1));
+        }
+
+        private void Update()
+        {
+            minusButton.interactable = _value > 0;
+            
+            valueText.text = _value.ToString();
+        }
+
+        public void Allocate(int points)
+        {
+            _value += points;
+            if (_value<0)
+            {
+                _value = 0;
+            }
+        }
+    }
+}
