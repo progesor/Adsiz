0299bda50512c812f3c7b2c63c521c05706010f8
Catlak Profesor <progesor@gmail.com>
2023-05-12 00:07:03 +0300

    Switchin UI Screens

---
+++ Assets/Scripts/UI/UISwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.UI
+{
+    public class UISwitcher : MonoBehaviour
+    {
+        [SerializeField] private GameObject entryPoint;
+
+        private void Start()
+        {
+            SwitchTo(entryPoint);
+        }
+
+        public void SwitchTo(GameObject toDisplay)
+        {
+            if (toDisplay.transform.parent != transform) return;
+            
+            foreach (Transform chiled in transform)
+            {
+                chiled.gameObject.SetActive(chiled.gameObject==toDisplay);
+            }
+        }
+    }
+}
