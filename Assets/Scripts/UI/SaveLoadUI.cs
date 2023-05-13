f8781c151e3a3b0311934fcc7e35a2199f4f9d61
Catlak Profesor <progesor@gmail.com>
2023-05-13 04:22:22 +0300

    Load Game UI

---
+++ Assets/Scripts/UI/SaveLoadUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.UI
+{
+    public class SaveLoadUI : MonoBehaviour
+    {
+        [SerializeField] private Transform contentRoot;
+        [SerializeField] private GameObject buttonPrefab;
+
+        private void OnEnable()
+        {
+            foreach (Transform child in contentRoot)
+            {
+                Destroy(child.gameObject);
+            }
+
+            Instantiate(buttonPrefab, contentRoot);
+            Instantiate(buttonPrefab, contentRoot);
+            Instantiate(buttonPrefab, contentRoot);
+        }
+    }
+}
