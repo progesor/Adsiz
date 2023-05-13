319d20c46fa5bb15cd2579b906785f9c55830629
Catlak Profesor <progesor@gmail.com>
2023-05-13 05:07:55 +0300

    Pausing With TimeScale

---
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ProgesorCreating.Control;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace ProgesorCreating.UI
+{
+    public class PauseMenuUI : MonoBehaviour
+    {
+        private PlayerController _playerController;
+        private void Start()
+        {
+            _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+
+        private void OnEnable()
+        {
+            Time.timeScale = 0;
+            _playerController.gameObject.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+            _playerController.gameObject.SetActive(true);
+        }
+    }
+}
